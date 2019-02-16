43bd8f27148666eccc0fbf6394b5a2f466f10aa9
taiga39 <ishikawa.tigers@gmail.com>
2019-02-16 18:08:59 +0900

    /hello でview

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/hello', 'WelcomeController@index');
